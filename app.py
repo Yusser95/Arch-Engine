e00975393530fecffdc8a047dc9c799af8862fae
Yusser95 <yasoooralghusain@gmail.com>
2019-05-02 14:44:31 +0300

    V2

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ login_manager.init_app(app)
 from models import *
 
 # c
-db.create_all()
+# db.create_all()
  
 # ScheduleLog.__table__.create(db.session.bind)
 # GeneratedRecipesIngrProductModel.__table__.create(db.session.bind)
