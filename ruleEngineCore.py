06ea5d375db1d512c7356a1c8678294cbc26ee29
Yusser95 <yasoooralghusain@gmail.com>
2019-05-06 18:40:47 +0300

    V2

--- ruleEngineCore.py
+++ ruleEngineCore.py
@@ -26,7 +26,7 @@ class DynamicRuleEngine():
         attributs = ""
         exec_env = "{}"
         if obj.parms:
-            attributs += ",".join(["{} = '{}'".format(i.param.name ,i.value) for i in obj.parms])
+            attributs += ", ".join(["{} = '{}'".format(i.param.name ,i.value) for i in obj.parms])
         if obj.childs:
 
             chids_Arrays = {}
@@ -37,13 +37,13 @@ class DynamicRuleEngine():
             for i in obj.childs:
                 chids_Arrays[i.object_type.name+"s"].append(i.name)
 
-            attributs += "," + ",".join(["{} = {}".format(k,str("["+",".join(chids_Arrays[k])+"]")) for k in chids_Arrays])
-            attributs +="," + ",".join(["{} = {}".format(i.name ,i.name) for i in obj.childs])
+            attributs += ", " + ", ".join(["{} = {}".format(k,str("["+",".join(chids_Arrays[k])+"]")) for k in chids_Arrays])
+            attributs +=", " + ", ".join(["{} = {}".format(i.name ,i.name) for i in obj.childs])
 
             exec_env = "{"+",".join(["'{}' : {}".format(i.name ,i.name) for i in obj.childs])+"}" #str({i.name:i.name for i in obj.childs})
 
         
-        attributs += ',{} = "{}"'.format("att_str",str(r"\n"+attributs.replace(",",r"\n")+r"\n"))
+        attributs += ',{} = "{}"'.format("att_str",str(r"\n"+attributs.replace(", ",r"\n")+r"\n"))
         attributs += ",{} = '{}'".format("logging",r"""\ninner_logs=[]\ndef add_to_log(error):\n\tinner_logs.append(error)\n""")
         attributs += ',{} = {}'.format("exec_env",exec_env)
 
